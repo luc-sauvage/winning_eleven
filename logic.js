0ebdb14b092e2ebc31e8758a5cbeccb50e8084d4
Luca Dalmasso <lucadalmasso@gmail.com>
2020-10-19 20:02:23 +0200

    slight changes

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
-function logic (playersInfos, matchDay) {
+function logic(playersInfos, matchDay) {
     // console.log("stats", stats);
-    
+
     const weight = {
         rating: 1,
         averageGoals: 9,
@@ -8,46 +8,42 @@ function logic (playersInfos, matchDay) {
         averageDribblings: 1,
         averageYellowCards: -1.5,
         averageRedCards: -3,
-        lineupLikelyhood: 1.5, 
+        lineupLikelyhood: 1.5,
         averageMissedPenalties: -9,
-    }
-
-
+    };
 
     function calculateFinalPoints(statistics) {
         // console.log("weight.rating", weight.rating);
         // console.log("statistics: ", statistics);
         // console.log("matchDay:", matchDay);
-        const weightedResult = (
-            (statistics.games.rating * weight.rating) + 
+        const weightedResult =
+            statistics.games.rating * weight.rating +
             (statistics.goals.total / matchDay) * weight.averageGoals +
             (statistics.goals.assists / matchDay) * weight.averageAssists +
-            (statistics.dribbles.success / statistics.dribbles.attempts) * weight.averageDribblings + 
+            (statistics.dribbles.success / statistics.dribbles.attempts) *
+                weight.averageDribblings +
             (statistics.cards.yellow / matchDay) * weight.averageYellowCards +
-            (statistics.cards.yellow / matchDay) * weight.averageRedCards + 
-            (statistics.games.lineups / matchDay) * weight.lineupLikelyhood + 
-            (statistics.penalty.missed / statistics.games.appearences) * weight.averageMissedPenalties);
-            
+            (statistics.cards.yellow / matchDay) * weight.averageRedCards +
+            (statistics.games.lineups / matchDay) * weight.lineupLikelyhood +
+            (statistics.penalty.missed / statistics.games.appearences) *
+                weight.averageMissedPenalties;
+
         return weightedResult;
-    };
+    }
 
-
-
+    // qui da verificare! perché non arriva più un json ma un array di oggetti ad un livello
     const resultsArr = playersInfos.map((playerInfo, i) => {
         // console.log(stat.player);
         const finalPoints = calculateFinalPoints(playerInfo.statistics[0]);
         const resultObj = {
             ...playerInfo,
             totalPoints: finalPoints,
-        }
+        };
         console.log(resultObj);
         return resultObj;
+    });
 
-
-    })
-    
-    return(resultsArr); 
-
-};
+    return resultsArr;
+}
 
 module.exports.logic = logic;
