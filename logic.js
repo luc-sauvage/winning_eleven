650b514875e45d577c27cc7e4293c1f3b9e303f0
Luca Dalmasso <lucadalmasso@gmail.com>
2020-10-22 15:06:10 +0200

    commit before creating last component: player stats popup

--- logic.js
+++ logic.js
@@ -17,37 +17,49 @@ function logic(playersInfos, matchDay) {
         averageFouls: -0.3,
         averageTackles: 0.7,
         averageInterceptions: 1,
-        wonDuelsRate: 2, 
+        wonDuelsRate: 2,
         averagePasses: 0.05,
         averageKeyPasses: 1,
         averageDrawnFouls: 0.3,
         averageWonPenalties: 1,
     };
 
-    function commonLogic (statistics) {
-        return statistics.rating * weight.rating +
-        (statistics.total_goals / statistics.match_day) *
-            weight.averageGoals +
-        (statistics.assists / statistics.match_day) *
-            weight.averageAssists +
-        ((statistics.success_dribbles / statistics.attempted_dribbles) *
-            weight.averageDribblings || 0) +
-        (statistics.yellow_cards / statistics.match_day) *
-            weight.averageYellowCards +
-        (statistics.red_cards / statistics.match_day) *
-            weight.averageRedCards +
-        (statistics.lineups / statistics.match_day) *
-            weight.lineupLikelyhood +
-        ((statistics.missed_penalties + statistics.scored_penalties) > 0 ? (statistics.missed_penalties / (statistics.missed_penalties + statistics.scored_penalties)) * weight.averageMissedPenalties : 0) +
-        statistics.total_interceptions / statistics.match_day * weight.averageInterceptions + 
-        ((statistics.won_duels / statistics.total_duels) * weight.wonDuelsRate) || 0 +
-        statistics.key_passes / statistics.match_day * weight.averageKeyPasses +
-        statistics.drawn_fouls / statistics.match_day * weight.averageDrawnFouls +
-        statistics.commited_penalties / statistics.match_day * weight.averageCommittedPenalties + 
-        statistics.won_penalties / statistics.match_day * weight.averageWonPenalties;
+    function commonLogic(statistics) {
+        return (
+            statistics.rating * weight.rating +
+                (statistics.total_goals / statistics.match_day) *
+                    weight.averageGoals +
+                (statistics.assists / statistics.match_day) *
+                    weight.averageAssists +
+                ((statistics.success_dribbles / statistics.attempted_dribbles) *
+                    weight.averageDribblings || 0) +
+                (statistics.yellow_cards / statistics.match_day) *
+                    weight.averageYellowCards +
+                (statistics.red_cards / statistics.match_day) *
+                    weight.averageRedCards +
+                (statistics.lineups / statistics.match_day) *
+                    weight.lineupLikelyhood +
+                (statistics.missed_penalties + statistics.scored_penalties > 0
+                    ? (statistics.missed_penalties /
+                          (statistics.missed_penalties +
+                              statistics.scored_penalties)) *
+                      weight.averageMissedPenalties
+                    : 0) +
+                (statistics.total_interceptions / statistics.match_day) *
+                    weight.averageInterceptions +
+                (statistics.won_duels / statistics.total_duels) *
+                    weight.wonDuelsRate ||
+            0 +
+                (statistics.key_passes / statistics.match_day) *
+                    weight.averageKeyPasses +
+                (statistics.drawn_fouls / statistics.match_day) *
+                    weight.averageDrawnFouls +
+                (statistics.commited_penalties / statistics.match_day) *
+                    weight.averageCommittedPenalties +
+                (statistics.won_penalties / statistics.match_day) *
+                    weight.averageWonPenalties
+        );
     }
-
-    
 
     function calculateFinalPoints(statistics) {
         // console.log("weight.rating", weight.rating);
@@ -56,40 +68,46 @@ function logic(playersInfos, matchDay) {
         let weightedResult;
 
         if (statistics.position === "Goalkeeper") {
-            weightedResult = 
-            statistics.rating * weight.rating +
-            statistics.conceded_goals / statistics.match_day * weight.averageConcededGoals +
-            statistics.yellow_cards / statistics.match_day *
-                weight.averageYellowCards + 
-            (statistics.red_cards / statistics.match_day) *
-                weight.averageRedCards +
-            (statistics.lineups / statistics.match_day) *
-                weight.lineupLikelyhood +
-            statistics.saved_penalties / statistics.match_day * weight.averageSavedPenalties +
-            statistics.saves / statistics.match_day * weight.averageSaves +
-            statistics.commited_penalties / statistics.match_day * weight.averageCommittedPenalties;
-        } 
-        else if  (statistics.position === "Defender") {
-             weightedResult = commonLogic(statistics) + 
-             statistics.committed_fouls / statistics.match_day * weight.averageFouls +
-             statistics.total_tackles / statistics.match_day * weight.averageTackles;
-        }
-
-        else if  (statistics.position === "Midfielder") {
-             weightedResult =  commonLogic(statistics) + 
-             statistics.committed_fouls / statistics.match_day * weight.averageFouls + 
-             statistics.total_tackles / statistics.match_day * weight.averageTackles + 
-             statistics.total_passes / statistics.match_day * weight.averagePasses ;
-        }
-
-        else {
-             weightedResult =  commonLogic(statistics);
+            weightedResult =
+                statistics.rating * weight.rating +
+                (statistics.conceded_goals / statistics.match_day) *
+                    weight.averageConcededGoals +
+                (statistics.yellow_cards / statistics.match_day) *
+                    weight.averageYellowCards +
+                (statistics.red_cards / statistics.match_day) *
+                    weight.averageRedCards +
+                (statistics.lineups / statistics.match_day) *
+                    weight.lineupLikelyhood +
+                (statistics.saved_penalties / statistics.match_day) *
+                    weight.averageSavedPenalties +
+                (statistics.saves / statistics.match_day) *
+                    weight.averageSaves +
+                (statistics.commited_penalties / statistics.match_day) *
+                    weight.averageCommittedPenalties;
+        } else if (statistics.position === "Defender") {
+            weightedResult =
+                commonLogic(statistics) +
+                (statistics.committed_fouls / statistics.match_day) *
+                    weight.averageFouls +
+                (statistics.total_tackles / statistics.match_day) *
+                    weight.averageTackles;
+        } else if (statistics.position === "Midfielder") {
+            weightedResult =
+                commonLogic(statistics) +
+                (statistics.committed_fouls / statistics.match_day) *
+                    weight.averageFouls +
+                (statistics.total_tackles / statistics.match_day) *
+                    weight.averageTackles +
+                (statistics.total_passes / statistics.match_day) *
+                    weight.averagePasses;
+        } else {
+            weightedResult = commonLogic(statistics);
         }
 
         /* console.log("weightedResult: ", weightedResult); */
-        console.log("!!!!!!!!!!!!!! statistics.missed_penalties", statistics.missed_penalties) ;
+        /* console.log("!!!!!!!!!!!!!! statistics.missed_penalties", statistics.missed_penalties) ;
         console.log("!!!!!!!!!!!!!! statistics.scored_penalties", statistics.scored_penalties) ;
-        console.log("!!!!!!!!!!!!!!", (statistics.missed_penalties / (statistics.missed_penalties + statistics.scored_penalties)) * weight.averageMissedPenalties || 0) ;
+        console.log("!!!!!!!!!!!!!!", (statistics.missed_penalties / (statistics.missed_penalties + statistics.scored_penalties)) * weight.averageMissedPenalties || 0) ; */
         return weightedResult;
     }
 
